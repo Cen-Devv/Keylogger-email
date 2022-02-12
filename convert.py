8e274df9c15941fd8badf879e165fec9fcc8e619
Cen-dev <99365013+Cen-Devv@users.noreply.github.com>
2022-02-12 17:20:37 -0400

    Update convert.py

--- convert.py
+++ convert.py
@@ -7,7 +7,7 @@ if (sys.platform=="win32"):
 executables = [cx_Freeze.Executable('main.py', base=base)]
 
 cx_Freeze.setup(
-    name="man.py",
+    name="main",
     options={'build_exe': {'packages':['pynput','re','smtplib','email.mime.multipart',
     'email.mime.text','email.mime.base','email','schedule','time'],}},
 
